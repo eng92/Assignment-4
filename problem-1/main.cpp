2ad950c867736a9a04913be242c0ee32d7cffb8c
Muhammad Ali Sayed <44190311+eng92@users.noreply.github.com>
2018-12-19 01:04:47 +0200

    Update main.cpp

--- problem-1/main.cpp
+++ problem-1/main.cpp
@@ -2,5 +2,22 @@
 
 int **transpose(int **y, int n) {
     // Write your code here
-    return 0;
+    int **result = new  int*[n];
+	for (int i = 0; i < n; i++)
+	{
+		result[i] = new int[n];
+	}
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			result[j][i] = y[i][j];
+			 
+		}
+
+	}
+    return result ;
+    
+    
+    
 }
