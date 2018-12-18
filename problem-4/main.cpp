99bd208e3e1a036d1a42c0be283a5b91455b4ca2
Muhammad Ali Sayed <44190311+eng92@users.noreply.github.com>
2018-12-19 01:12:57 +0200

    Update main.cpp

--- problem-4/main.cpp
+++ problem-4/main.cpp
@@ -2,5 +2,29 @@
 
 int **multiplyMatrices(int **x, int **y, int n, int m) {
     // Write your code here
-    return 0;
+    	int **result = new  int*[n];
+	for (int i = 0; i < n; i++)
+	{
+		result[i] = new int[n];
+	}
+	int i, j, k;
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++){
+			result[i][j] = 0;
+		}
+	}
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j<n; j++)
+		{
+			for (k = 0; k < m; k++)
+			{
+				result[i][j] += x[i][k] * y[k][j];
+			}
+		}
+	}
+
+    return result;
+    
 }
