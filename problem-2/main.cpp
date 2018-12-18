04c61a7864f8c6fc72e5d8b3b95b0573a86c91c2
Muhammad Ali Sayed <44190311+eng92@users.noreply.github.com>
2018-12-19 01:07:59 +0200

    Update main.cpp

--- problem-2/main.cpp
+++ problem-2/main.cpp
@@ -3,10 +3,29 @@
 int multiply(const int x[], const int y[], int n) {
     int result = 0;
     // Copy code from problem 3 of assignment 3 here
-    return result;
+    for (int counter = 0; counter < n; counter++)
+	{
+		result += (x[counter] * y[counter]);
+	}
+    return result; 
+    
 }
 
 int *multiply(int x[], int *y[], int n) {
     // Write your code here
-    return 0;
+    
+    int **result = new  int*[n];
+	for (int i = 0; i < n; i++)
+	{
+		result[i] = new int[n];
+	}
+	y = transpose(y, n);
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			result += x[i] * y[i][j];
+		}
+	}
+    return  *result ;
 }
